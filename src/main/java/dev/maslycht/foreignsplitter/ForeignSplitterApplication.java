09ea3b7a2feb55f18b72b20859e1eb1ad95da737
Taras Maslych <taras.maslych@envelope.vision>
2025-02-11 15:12:57 -0500

    Initial commit

---
+++ src/main/java/dev/maslycht/foreignsplitter/ForeignSplitterApplication.java
@@ -0,0 +1,13 @@
+package dev.maslycht.foreignsplitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForeignSplitterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForeignSplitterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/maslycht/foreignsplitter/ForeignSplitterApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.maslycht.foreignsplitter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForeignSplitterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
